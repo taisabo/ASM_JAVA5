7ac8e19dbf4ce616e7806711dcce20f6003650df
taisabo <taincpc08925@gmail.com>
2025-03-05 22:02:51 +0700

    Update CheckOutController.java

--- src/main/java/com/fpoly/java5/controllers/CheckOutController.java
+++ src/main/java/com/fpoly/java5/controllers/CheckOutController.java
@@ -49,14 +49,13 @@ public class CheckOutController {
 
 	@PostMapping("/user/checkout")
 	public String checkout(@RequestParam("paymentMethod") int paymentMethod,
-			@RequestParam("selectedAddressId") int selectedAddressId, Model model) {
+			@RequestParam("selectedAddressId") int selectedAddressId, Model model)  {
 		try {
 
 			if (paymentMethod < 0 || paymentMethod > 2) {
 				model.addAttribute("error", "Phương thức thanh toán không hợp lệ.");
 				return "/user/checkout.html";
 			}
-
 			orderService.CreateOrder(2, paymentMethod);
 			return "redirect:/cart";
 		} catch (Exception e) {
