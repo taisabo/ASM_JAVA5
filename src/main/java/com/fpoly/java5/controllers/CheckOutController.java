96a3c3831178395844d0453f548f98760504e24c
taisabo <taincpc08925@gmail.com>
2025-03-05 21:51:04 +0700

    Update CheckOutController.java

--- src/main/java/com/fpoly/java5/controllers/CheckOutController.java
+++ src/main/java/com/fpoly/java5/controllers/CheckOutController.java
@@ -48,21 +48,14 @@ public class CheckOutController {
 	}
 
 	@PostMapping("/user/checkout")
-	public String checkout(
-<<<<<<< HEAD
-			@RequestParam("paymentMethod") int paymentMethod, 
-			@RequestParam("selectedAddressId") int selectedAddressId,
-			Model model) {
-=======
-			@RequestParam("paymentMethod") int paymentMethod, Model model) {
->>>>>>> parent of b094af6 (profile-user)
+	public String checkout(@RequestParam("paymentMethod") int paymentMethod,
+			@RequestParam("selectedAddressId") int selectedAddressId, Model model) {
 		try {
-			
-			
+
 			if (paymentMethod < 0 || paymentMethod > 2) {
-	            model.addAttribute("error", "Phương thức thanh toán không hợp lệ.");
-	            return "/user/checkout.html";
-	        }
+				model.addAttribute("error", "Phương thức thanh toán không hợp lệ.");
+				return "/user/checkout.html";
+			}
 
 			orderService.CreateOrder(2, paymentMethod);
 			return "redirect:/cart";
